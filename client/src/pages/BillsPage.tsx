51dbbb7121b26b275fdd15acc005c773eaf708a3
Aliyan ahmed Azad <aliyanahmed853@gmail.com>
2024-04-23 22:48:58 +0500

    aqswa

--- client/src/pages/BillsPage.tsx
+++ client/src/pages/BillsPage.tsx
@@ -4,7 +4,7 @@ import DefaultLayout from '../components/DefaultLayout'
 const BillsPage:React.FC = () => {
   return (
     <DefaultLayout>
-      <h1>Bills Page ultra for pushing need to keep the streak</h1>
+      <h1>Bills Page ultra for pushing need to keep the streak going</h1>
     </DefaultLayout>
   )
 }
