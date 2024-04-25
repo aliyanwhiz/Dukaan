367943e39677daa57f3f9f89329b35744be5d8f1
Aliyan ahmed Azad <aliyanahmed853@gmail.com>
2024-04-25 20:02:55 +0500

    majboori push

--- client/src/pages/BillsPage.tsx
+++ client/src/pages/BillsPage.tsx
@@ -4,7 +4,7 @@ import DefaultLayout from '../components/DefaultLayout'
 const BillsPage:React.FC = () => {
   return (
     <DefaultLayout>
-      <h1>Bills Page</h1>
+      <h1>Bills Page Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis, quos.</h1>
     </DefaultLayout>
   )
 }
